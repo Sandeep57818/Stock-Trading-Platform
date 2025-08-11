d208dd4eebe695bbf2c7ef0ac502c0190de945a6
Sandeep-Sanju <sandeeptharalla7@gmail.com>
2025-08-11 23:39:12 +0530

    Home Components Updated Part 1

---
+++ src/landing_page/home/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Awards from './Awards';
+import Education from './Education';
+import Hero from './Hero';
+import Stats from './Stats';
+import Pricing from './Pricing';
+import OpenAccount from '../OpenAccount';
+import NavBar from '../Navbar';
+import Footer from '../Footer';
+function HomePage() {
+    return (
+       <>
+       <NavBar/>
+       <Hero/>
+       <Awards/>
+       <Stats/>
+       <Pricing/>
+       <Education/>
+       <OpenAccount/>
+       <Footer />
+       
+       </>
+      );
+}
+
+export default HomePage;
