bfa8b42b1c5fc70068b636c91d1470b67a60a976
Sandeep-Sanju <sandeeptharalla7@gmail.com>
2025-08-20 23:57:45 +0530

    Pricing component updated

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -1,45 +1,35 @@
 import React from 'react'
 
 
-function Hero () {
+function Hero() {
     return (
-                <div className='container mt-5'>
-            <div className='row p-5'>
-                <h1 className='fs-4 text-center text-muted'>
-                    We poineered the discount broking model in India.<br />
-                    Now, we are breaking with our technology.
-                </h1>
-            </div>
-            <div className='row p- mt-5 border-top text-muted'
-             style={{lineHeight:"2.1", fontSize:"1.2em"}}
-            >
-                <div className='col-6 p-5'>
-                    <p>
-                        We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. We named the company Zerodha, a combination of Zero and "Rodha", the Sanskrit word for barrier.
-                        </p>
-                        <p>
-                        Today, our disruptive pricing models and in-house technology have made us the biggest stock broker in India.
-                    </p> 
-                    <p>
-                        Over 1.6+ crore clients place billions of orders every year through our powerful ecosystem of investment platforms, contributing over 15% of all Indian retail trading volumes.
-                    </p>
-                </div>
-                <div className='col-6 p-5'>
-                    <p>
-                        In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.
-                    </p>
-                    <p>
-                        <a href=""style={{textDecoration:"none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.
-                    </p> 
-
-                    <p>
-                        And yet, we are always up to something new every day. Catch up on the latest updates on our <a href=''style={{textDecoration:"none"}}>blog</a> or see what the media is saying about us or learn more about our business and product philosophies.
-                    </p>
-                </div>
-
+        <div className='container mt-5'>
+            <div className='row p-5 mt-5 border-bottom  text-center'>
+                <h1>Pricing</h1>
+                <h3 className='text-muted fs-5 mt-3'>Free equity investment and flat ₹20 traday and F&O trades</h3>
             </div>
 
+
+            <div className='row p-5 text-center'>
+             <div className='col-4 p-5'>
+                <img src="media/images/pricingEquity.svg" />
+                <h1>Free equity delivery</h1>
+                <p className='text-muted mt-2'>All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+             </div>
+             <div className='col-4 p-5'>
+                 <img src="media/images/intradayTrades.svg" />
+                <h1>Intraday and F&O trades</h1>
+                <p className='text-muted mt-2' >Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p> </div>
+             <div className='col-4 p-5'>
+                 <img src="media/images/pricingEquity.svg" />
+                <h1>Free direct MF</h1>
+                <p className='text-muted mt-2'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p> </div>
+
+               </div>
         </div>
+
+
+
     )
 }
 export default Hero;
