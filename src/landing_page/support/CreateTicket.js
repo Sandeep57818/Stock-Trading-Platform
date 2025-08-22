4d6d291ee70383df7a856b19a2240d94e6e40719
Sandeep-Sanju <sandeeptharalla7@gmail.com>
2025-08-22 20:10:17 +0530

    Updated the Support component

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -5,19 +5,70 @@ function CreateTicket() {
     return (
         <div className='container'>
             <div className='row p-5 mt-5 mb-5'>
-                <h3>To create a ticket, select a relecant topic</h3>
-                <div className='col-4 p-5 mt-5 mb-5'>
+                <h3 className='mb-2'>To create a ticket, select a relecant topic</h3>
+                <div className='col-4 p-5 mt-2 mb-2'>
                     <h5 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h5>
-                    
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Online Account Opening</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Offline Account Opening</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Company, Patnership and HUF Account</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}> Opening</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>NRI Account Opening</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Charges at Zerodha</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Get Started</a><br />
+
                 </div>
-                <div className='col-4 p-5 mt-5 mb-5'>
+                <div className='col-4 p-5 mt-2 mb-2'>
                     <h5 className=''><i class="fa fa-user" aria-hidden="true"></i> Your Zerodha Account</h5>
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Login Credentials</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Account Modification and Segment Addition</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>DP ID and bank details</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}> Your Profile</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Transfer and conversion of shares</a><br />
+
                 </div>
-                <div className='col-4 p-5 mt-5 mb-5'>
+                <div className='col-4 p-5 mt-2 mb-2'>
                     <h5 className=''><i class="fa fa-bar-chart" aria-hidden="true"></i> Your Zerodha Account</h5>
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>MArgin/Leverage, Product and Order types</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Kite Web and Mobile</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Trading FAQS</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}> Corporate Actions</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Sentinel</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Kite API</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Pi and other platform</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>GTT</a><br />
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h5 className=''><i class="fa fa-credit-card" aria-hidden="true"></i>  Funds </h5>
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Adding Funds</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Fund Withdrawl</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>eMandates</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Adding Bank Accounts</a><br />
+
+
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h5 className=''><i class="fa fa-circle-o-notch" aria-hidden="true"></i> Console</h5>
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Reports</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>ledger</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Portfolio</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}> 60 Day Challenge</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>IPO</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Referral Program</a><br />
+
+                </div>
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h5 className=''><i class="fa fa-circle-o" aria-hidden="true"></i> Coin</h5>
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Understanding Mutual Funds</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>About Coin</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Buying and Selling through Coin </a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}> Starting an SIP</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Managing your Portfoilo</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Coin App</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Moving to Coin</a><br />
+                    <a href='' style={{ textDecoration: "none", lineHeight: "2.5" }}>Government Securities</a><br />
                 </div>
             </div>
-
         </div>
     )
 }
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
 
-function Hero () {
+function Hero() {
     return (
-        <>
-        <h1>Hero</h1>
-        </>
+        <section className='container-fluid' id="supportHero">
+            <div className='mt-5  p-4' id="supportWrapper">
+                <h4>Support Portal</h4>
+                <a className='move-left' href="" style={{ color: "white" }}>Track Tickets</a>
+            </div>
+            <div className='row move-right'>
+                <div className='col-6 p-2   my-3'>
+                    <h1 className='fs-5'>Search for an answer or browse help topics <br/>to create a ticket</h1>
+                    <input className='fs-6' placeholder='Eg. How do I activate F&O, why is my order getting reject' /><br />
+                    <a href="" style={{ textDecoration: "non", color: "white" }} className='me-3'>Track Account Opening</a>
+                    <a href="" style={{ textDecoration: "non", color: "white" }}className='me-3'>Track segment activation</a>
+                    <a href="" style={{ textDecoration: "noe", color: "white" }}className='me-3'>Intraday margins</a> <br />
+                    <a href="" style={{ textDecoration: "noe", color: "white" }}className='me-3'>Kite user manual</a> <br />
+                </div>
+                <div className='col-6 p-2 ms-auto' >
+                    <h2 className='fs-5'>Featured</h2>
+                    <ol>
+                        <li><a href="" style={{ color: "white" }}>Current TakeOvers and Delisting - August 2025</a>
+                        </li>
+                        <li><a href="" style={{ color: "white" }}>Latest Intraday Leverages - MIS & CO</a>
+                        </li>
+                    </ol>
+
+
+                </div>
+
+            </div>
+
+        </section>
     )
 }
 export default Hero;
