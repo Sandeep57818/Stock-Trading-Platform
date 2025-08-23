e09514bef9d2a9d566f31549e97e8d7d0d40abac
Sandeep-Sanju <sandeeptharalla7@gmail.com>
2025-08-23 13:10:12 +0530

    Creating the Dashboard component

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -13,16 +13,16 @@ function Hero() {
             <div className='row p-5 text-center'>
              <div className='col-4 p-5'>
                 <img src="media/images/pricingEquity.svg" />
-                <h1>Free equity delivery</h1>
+                <h1 className='fs-3'>Free equity delivery</h1>
                 <p className='text-muted mt-2'>All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
              </div>
              <div className='col-4 p-5'>
                  <img src="media/images/intradayTrades.svg" />
-                <h1>Intraday and F&O trades</h1>
+                <h1 className='fs-3'>Intraday and F&O trades</h1>
                 <p className='text-muted mt-2' >Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p> </div>
              <div className='col-4 p-5'>
                  <img src="media/images/pricingEquity.svg" />
-                <h1>Free direct MF</h1>
+                <h1 className='fs-3'>Free direct MF</h1>
                 <p className='text-muted mt-2'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p> </div>
 
                </div>
